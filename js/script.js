96d610d3603e31d340f8cffa8e31bffb6d629eae
Rafael <rafaelbritovaladares@gmail.com>
2025-06-11 15:34:04 -0300

    Responsividade e JavaScript para mudar o tema adicionados

---
+++ js/script.js
@@ -0,0 +1,4 @@
+/* ---------------- Animação digitando ---------------- */
+var typed = new Typed(".typing",{
+    strings:["Desenvolvedor back-end", "Desenvolvedor Web", "Testador de software"]
+})
